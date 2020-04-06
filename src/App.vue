55c626945d22554427b704642cefb89ef9f45048
Jhersy <jhersyvb@gmail.com>
2020-04-06 16:41:41 -0500

    4 Add a bill category

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <NavBar />
-    <div class="container flex">
-      <div class="w-1/2">
-        <BillsTable />
-      </div>
-      <div class="w-1/2">
-        <Chart :bills="activeBills" />
+    <AddCategory v-if="shouldShowAddCategory" @addCategory="addCategory" />
+    <div v-else>
+      <NavBar />
+      <div class="container flex">
+        <div class="w-1/2">
+          <BillsTable />
+        </div>
+        <div class="w-1/2">
+          <Chart :bills="activeBills" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import AddCategory from './components/AddCategory'
+import AddCategory from './components/AddCategory'
 // import AddBill from './components/AddBill'
 import NavBar from './components/NavBar'
 import Chart from './components/Chart'
@@ -23,11 +26,26 @@ export default {
   name: 'App',
 
   components: {
-    // AddCategory,
+    AddCategory,
     // AddBill,
     Chart,
     BillsTable,
     NavBar
+  },
+
+  data() {
+    return {
+      bills: [],
+      categories: [],
+      shouldShowAddCategory: true
+    }
+  },
+
+  methods: {
+    addCategory(category) {
+      this.categories.push(category)
+      this.shouldShowAddCategory = false
+    }
   }
 }
 </script>
--- src/components/AddCategory.vue
+++ src/components/AddCategory.vue
@@ -1,3 +1,44 @@
 <template>
-  <div>Add category</div>
+  <div class="h-100 w-full flex items-center justify-center font-sans">
+    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+      <div class="mb-4">
+        <h1 class="text-4xl mb-2 text-gray-900">Enter a category of bills</h1>
+        <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
+        <div class="flex mt-4">
+          <input
+            v-model="category"
+            placeholder="Add category"
+            class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          <button
+            class="flex-no-shrink p-2 rounded bg-teal-500 text-white hover:bg-teal-600 focus:outline-none focus:shadow-outline"
+            @click="handleClick"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      category: ''
+    }
+  },
+
+  methods: {
+    handleClick() {
+      if (!this.category) {
+        alert('Enter a category')
+        return
+      }
+
+      this.$emit('addCategory', this.category)
+    }
+  }
+}
+</script>
